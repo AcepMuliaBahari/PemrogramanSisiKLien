3fc0df505569bb3b045aee993d418301aa94d0f1
unknown <acep.bahari1995@gmail.com>
2024-06-22 13:16:57 +0700

    CRUD

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -2,10 +2,15 @@ import "./single.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import Chart from "../../components/chart/Chart";
-import List from "../../components/datatable/Datatable";
+import Datatable from "../../components/datatable/Datatable";
+import { useLocation } from "react-router-dom";
 
-const Single = () => {
-  return (
+const Single = ({columns}) => {
+    const location = useLocation();
+    const id = location.pathname.split('/')[2];
+
+
+  return (  
     <div className="single">
       <Sidebar />
       <div className="singleContainer">
@@ -23,8 +28,8 @@ const Single = () => {
               <div className="details">
                 <h1 className="itemTitle">Jane Doe</h1>
                 <div className="detailItem">
-                  <span className="itemKey">Email:</span>
-                  <span className="itemValue">janedoe@gmail.com</span>
+                  <span className="itemKey">ID:</span>
+                  <span className="itemValue"> {id} </span>
                 </div>
                 <div className="detailItem">
                   <span className="itemKey">Phone:</span>
@@ -47,9 +52,9 @@ const Single = () => {
             <Chart aspect={3 / 1} type="order" />
           </div>
         </div>
-        <div className="bottom">
+        <div className="bottom">  
           <h1 className="title">Last Transactions</h1>
-          <List />
+          <Datatable columns={columns} />
         </div>
       </div>
     </div>
